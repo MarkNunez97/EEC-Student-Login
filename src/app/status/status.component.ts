fa25cc2acb4851d2716bb52c27f7de6474979125
marknunez97 <marknunez1997@gmail.com>
2019-12-02 12:41:39 -0700

    completed gui for student users

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-status',
@@ -7,14 +8,36 @@ import {Router} from '@angular/router';
   styleUrls: ['./status.component.css']
 })
 export class StatusComponent implements OnInit {
-
+  value = 0;
   footer:any;
+  //  0/3 = 0
+  //  1/3 = 33
+  //  2/3 = 67
+  //  3/3 = 100
+  
 
   constructor(private router: Router) { }
-  value = 70;
+ 
+  curSec: number = 0;
+
+  startTimer(seconds: number) {
+    const time = seconds;
+    const timer$ = interval(1000);
+
+    const sub = timer$.subscribe((sec) => {
+      this.value = 0 + sec * 100 / seconds;
+      this.curSec = sec;
+
+      if (this.curSec === seconds) {
+        sub.unsubscribe();
+      }
+    });
+  }
 
   ngOnInit() {
+    this.startTimer(1);
   }
+
 
   ngAfterViewInit() {
     this.footer = document.getElementById('footer');
@@ -31,4 +54,5 @@ export class StatusComponent implements OnInit {
   goToStatus(){
     this.router.navigate(["status"]);
   }
+
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,26 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  
+  footer:any;
+
+  forms: Section[] = [
+    {
+      name: 'Advising Form: Submited',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Student Travel Form: Under Review',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Change of Major Form: Completed',
+      updated: new Date('1/28/16'),
+    }
+  ];
+
   constructor(private router: Router) { 
     
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.footer = document.getElementById('footer');
   }
 
   goToAbout(){
     this.router.navigate(["about"]);
   }
 
+  
   goToContact(){
+    this.footer.scrollIntoView();
   }
   
-  ngOnInit() {
-  }
 
   
 }
